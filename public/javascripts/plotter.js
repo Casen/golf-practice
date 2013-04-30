1538c36c652fd0f021e8d91ad5b503725b40e4e4
Casen <cdubd10@gmail.com>
2013-04-30 00:52:24 -0400

    fixed bug with plotter

--- public/javascripts/plotter.js
+++ public/javascripts/plotter.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var path = window.location.href.replace('swings', 'charts');
+  var path = window.location.href.replace('swings', 'data');
   $.get(path, function(points){
     $.jqplot('chart', points, {axes:{xaxis:{min: 50, max:300, numberTicks: 9}, yaxis:{min:-150, max: 150, numberTicks: 9}}, series:[{showLine: false}]});
   });
