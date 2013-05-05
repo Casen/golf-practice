8142588c7d7e3b80de72768e19d62b0b0235ffcf
Casen <cdubd10@gmail.com>
2013-05-05 18:05:30 -0400

    updating csv upload processor

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var _ = require('underscore')
 var csvToJSON = require('csv_to_json');
 var db = require('../db').db;
 /*
@@ -19,9 +20,25 @@ exports.processCSV = function(req, res){
     console.log(newPath);
 
     fs.writeFile(newPath, data, function(err){
-      csvToJSON.convert(newPath.replace('.csv', ''));
-      var swings = require(newPath.replace('.csv', '.json'));
-      db.swings.insert(swings);
+      csvToJSON.convert(newPath, function(jsonFile){
+        var swings = require(jsonFile);
+        var processed = _.map(swings, function(swing){
+          var processedSwing = {};
+          _.each(swing, function(val, key){
+            if(typeof(val) !== "object" && val !== "N/A") {
+              key = key.toLowerCase().replace(/\s/g, '_');
+              processedSwing[key] = val;
+            }
+          });
+          processedSwing.created_at = new Date();
+          return processedSwing;
+        });
+        processed = _.filter(processed, function(swing){
+          return swing.club !== "Average";
+        });
+
+        db.swings.insert(processed);
+      });
     });
   });
 };
