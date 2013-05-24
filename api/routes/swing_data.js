62048aa60a7a08daf58c9ee2f1c2db9e614bb69f
Casen <cdubd10@gmail.com>
2013-05-24 18:07:04 -0400

    adding the ability to create a time series based on 10 shot groupings

--- api/routes/swing_data.js
+++ api/routes/swing_data.js
@@ -13,5 +13,15 @@ exports.clubs = function(req, res) {
       res.send(swingCollection.analytics());
     }
   });
+}
 
+exports.stat_over_time = function(req, res) {
+  swingCollection.fetch(req.params.club, {},{
+    error: function(error){
+      console.log(error);
+    },
+    success: function(swings) {
+      res.send(swingCollection.stat_over_time(req.params.stat));
+    }
+  });
 }
